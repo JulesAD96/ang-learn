ba78b34e53cbfae924fcd1eff61e0b2d8afc177e
Jules <adonsijules9@gmail.com>
2023-01-05 14:44:34 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:"", pathMatch:'full', redirectTo:'login'},
+  { path: 'auth', component: AuthComponent},
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
